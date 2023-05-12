1132eeb31a9cec9e693fdd54a3a000ced6e7206e
ardhimaulidani <ardhi.maulidani@gmail.com>
2023-05-12 16:11:36 +0700

    add Path & Pose callback

--- src/asr_its/control_layout.cpp
+++ src/asr_its/control_layout.cpp
@@ -7,27 +7,31 @@ Robot::Robot(): RosRate(100)
     ROS_INFO("Robot Main Controller");
     
     // Subscriber & Publisher
-    SubJoy          = Nh.subscribe("/joy", 18, &Robot::JoyCallback, this);
-    SubJoyBattery   = Nh.subscribe("/battery", 10, &Robot::JoyBatteryCallback, this);
-    PubSpeed        = Nh.advertise<main_controller::ControllerData>("robot/cmd_vel", 10);
-    PubJoyFeedback  = Nh.advertise<sensor_msgs::JoyFeedbackArray>("/set_feedback", 10);
+    Sub_Joy            = Nh.subscribe("/joy", 18, &Robot::Joy_Callback, this);
+    Sub_Joy_Battery    = Nh.subscribe("/battery", 10, &Robot::Joy_Battery_Callback, this);
+
+    Sub_Path           = Nh.subscribe("/path", 10, &Robot::Path_Callback, this);
+    Sub_Pose           = Nh.subscribe("/amcl_pose", 10, &Robot::Pose_Callback, this);
+
+    Pub_Vel            = Nh.advertise<main_controller::ControllerData>("robot/cmd_vel", 10);
+    Pub_Joy_Feedback   = Nh.advertise<sensor_msgs::JoyFeedbackArray>("/set_feedback", 10);
 
     // Initialize Speed Variable
     for (int i = 0; i <=2 ; i++){
-        MsgSpeed.data.push_back(0);
+        vel_msg.data.push_back(0);
     }
 
     // Initialize Joystick LED Feedback
-    MsgJoyLED_R.type    = 0;
-    MsgJoyLED_R.id      = 0;
+    MsgJoyLED_R.type      = 0;
+    MsgJoyLED_R.id        = 0;
     MsgJoyLED_R.intensity = 0.0;
 
-    MsgJoyLED_G.type    = 0;
-    MsgJoyLED_G.id      = 1;
+    MsgJoyLED_G.type      = 0;
+    MsgJoyLED_G.id        = 1;
     MsgJoyLED_R.intensity = 0.0;
 
-    MsgJoyLED_B.type    = 0;
-    MsgJoyLED_B.id      = 2;
+    MsgJoyLED_B.type      = 0;
+    MsgJoyLED_B.id        = 2;
     MsgJoyLED_B.intensity = 0.0;
 
     MsgJoyFeedbackArray.array.push_back(MsgJoyLED_R);
@@ -35,23 +39,26 @@ Robot::Robot(): RosRate(100)
     MsgJoyFeedbackArray.array.push_back(MsgJoyLED_B);
 
     while(ros::ok()){
-        // Set Status Control using Triangle Button
+        
+        // Set Status Control using TRIANGLE Button
         if (Controller.Buttons[triangle] == 0 && Controller.prev_button[triangle] == 1)
         {
             StatusControl ^= 1;
-            MsgSpeed.StatusControl = StatusControl;
+            vel_msg.StatusControl = StatusControl;
         }
         Controller.prev_button[triangle] = Controller.Buttons[triangle];
 
         // Print Robot Speed to Screen
-        std::cout << "Speed[0] : " << RobotSpeed[0] << " Speed[1] : " << RobotSpeed[1] << " Speed[2] : " << RobotSpeed[2] << " Status : " << MsgSpeed.StatusControl << " Joystick Battery : " << JoyBatt*100 << "%" << std::endl;
+        std::cout << "Speed[0] : " << RobotSpeed[0] << " Speed[1] : " << RobotSpeed[1] << " Speed[2] : " << RobotSpeed[2] << " Status : " << vel_msg.StatusControl << " Joystick Battery : " << JoyBatt*100 << "%" << std::endl;
 
+        // Set Mode Using OPTIONS Button
         if (Controller.Buttons[OPTIONS] == 0 && Controller.prev_button[OPTIONS] == 1)
         {
             GuidedMode ^= 1;
         }
         Controller.prev_button[OPTIONS] = Controller.Buttons[OPTIONS];
 
+        // Go to Autonomous Mode
         if (GuidedMode)
         {
             // Set LED Feedback PURPLE for Guided Mode
@@ -61,14 +68,16 @@ Robot::Robot(): RosRate(100)
 
             // ***TO-DO : ADD PURE PURSUIT MODE*** //
             // Set Robot Speed to Zero (CHECK AGAIN FOR PURE PURSUIT IMPLEMENTATION)
-            for(int i = 0; i<3; i++)
+            for(int i = 0; i<=2; i++)
             {
                 RobotSpeed[i] = 0;
             }
         }
+
+        // Go to Manual Control Mode
         else
         {
-            if (MsgSpeed.StatusControl)
+            if (vel_msg.StatusControl)
             {
                 // Set LED Feedback RED for Joystick Lock Mode
                 MsgJoyLED_R.intensity = 0.12;
@@ -88,7 +97,7 @@ Robot::Robot(): RosRate(100)
                 MsgJoyLED_B.intensity = 0.13;     
 
                 // Set Robot Speed to Zero (CHECK AGAIN FOR PURE PURSUIT IMPLEMENTATION)
-                for(int i = 0; i<3; i++)
+                for(int i = 0; i<=2; i++)
                 {
                     RobotSpeed[i] = 0;
                 }     
@@ -98,7 +107,7 @@ Robot::Robot(): RosRate(100)
         // Push Robot Variable to Publisher Var
         for(int i = 0 ; i<=2 ; i++)
         {
-            MsgSpeed.data.at(i) = RobotSpeed[i];
+            vel_msg.data.at(i) = RobotSpeed[i];
         }
 
         MsgJoyFeedbackArray.array.at(0) = MsgJoyLED_R;
@@ -106,8 +115,8 @@ Robot::Robot(): RosRate(100)
         MsgJoyFeedbackArray.array.at(2) = MsgJoyLED_B;
 
         // Publish Topics
-        PubJoyFeedback.publish(MsgJoyFeedbackArray);
-        PubSpeed.publish(MsgSpeed);
+        Pub_Vel.publish(vel_msg);
+        Pub_Joy_Feedback.publish(MsgJoyFeedbackArray);
 
         ros::spinOnce(); 
         RosRate.sleep();
@@ -116,24 +125,36 @@ Robot::Robot(): RosRate(100)
 
 Robot::~Robot(){}
 
-void Robot::JoyCallback(const sensor_msgs::Joy::ConstPtr &MsgJoy)
+void Robot::Joy_Callback (const sensor_msgs::Joy::ConstPtr &joy_msg)
 {
     for(int i = 0; i<4; i++)
     {
-        Controller.Axis[i] = MsgJoy->axes[i];
+        Controller.Axis[i] = joy_msg->axes[i];
     }
 
     for(int i = 0; i<18; i++)
     {
-        Controller.Buttons[i] = MsgJoy->buttons[i];
+        Controller.Buttons[i] = joy_msg->buttons[i];
     }
 }
 
-void Robot::JoyBatteryCallback(const sensor_msgs::BatteryState::ConstPtr &MsgJoyBatt)
+void Robot::Joy_Battery_Callback (const sensor_msgs::BatteryState::ConstPtr &joy_batt_msg)
 {
-    JoyBatt = MsgJoyBatt->percentage;
+    JoyBatt = joy_batt_msg->percentage;
 }
 
-void Robot::OdomCallback(const nav_msgs::OdometryConstPtr &msg)
+void Robot::Path_Callback (const nav_msgs::Path::ConstPtr &path_msg)
 {
+    for(int i = 0; i < path_msg->poses.size(); ++i)
+    {
+        path.x.push(path_msg->poses[i].pose.position.x);
+        path.y.push(path_msg->poses[i].pose.position.y);
+        path.z.push(path_msg->poses[i].pose.position.z);       
+    }
+}
+void Robot::Pose_Callback (const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose_msg)
+{
+    robot_pose.x = pose_msg->pose.pose.position.x;
+    robot_pose.y = pose_msg->pose.pose.position.y;
+    robot_pose.z = pose_msg->pose.pose.position.z;    
 }
