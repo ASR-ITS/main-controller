1ed999385d95ce08314a119a684f6418bf4c3bf4
ardhimaulidani <ardhi.maulidani@gmail.com>
2023-05-13 18:37:13 +0700

    add path tracking

--- src/asr_its/control_layout.cpp
+++ src/asr_its/control_layout.cpp
@@ -38,6 +38,10 @@ Robot::Robot(): RosRate(100)
     MsgJoyFeedbackArray.array.push_back(MsgJoyLED_G);
     MsgJoyFeedbackArray.array.push_back(MsgJoyLED_B);
 
+    // Initialize Pure Purusit
+    Pose_t target_pose;
+    Pose_t output_speed;
+
     while(ros::ok()){
         
         // Set Status Control using TRIANGLE Button
@@ -67,16 +71,23 @@ Robot::Robot(): RosRate(100)
             MsgJoyLED_B.intensity = 0.53;
 
             // ***TO-DO : ADD PURE PURSUIT MODE*** //
-            // Set Robot Speed to Zero (CHECK AGAIN FOR PURE PURSUIT IMPLEMENTATION)
-            for(int i = 0; i<=2; i++)
-            {
-                RobotSpeed[i] = 0;
-            }
+            // Prevent going to origin if there's no path
+            if(path.x.size() <= 0)
+                target_pose = robot_pose;
+            else
+                target_pose = PurePursuit(robot_pose, path, 0.5);
+
+            output_speed = PointToPointPID(robot_pose, target_pose, 20);
+
+            RobotSpeed[0] = (int) output_speed.x;
+            RobotSpeed[1] = (int) output_speed.y;
+            RobotSpeed[2] = (int) output_speed.z;
         }
 
         // Go to Manual Control Mode
         else
         {
+            ClearPath(path);
             if (vel_msg.StatusControl)
             {
                 // Set LED Feedback RED for Joystick Lock Mode
@@ -152,9 +163,130 @@ void Robot::Path_Callback (const nav_msgs::Path::ConstPtr &path_msg)
         path.z.push(path_msg->poses[i].pose.position.z);       
     }
 }
+
 void Robot::Pose_Callback (const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose_msg)
 {
     robot_pose.x = pose_msg->pose.pose.position.x;
     robot_pose.y = pose_msg->pose.pose.position.y;
     robot_pose.z = pose_msg->pose.pose.position.z;    
+}
+
+void Robot::ClearPath(Path_t &path){
+    while(!path.x.empty()){
+        path.x.pop();
+        path.y.pop();
+        path.z.pop();
+        }
+    }
+
+Robot::Pose_t Robot::PurePursuit(Pose_t robotPose, Path_t &path, float offset)
+{
+
+    Pose_t targetPose;
+    targetPose = robotPose;
+
+    float distance = 0;
+    static float epsilon = 0.1;
+    int pathLeft = path.x.size();
+
+    targetPose.x = path.x.front();
+    targetPose.y = path.y.front();
+    targetPose.z = path.z.front();
+    distance = sqrt(pow((targetPose.x - robotPose.x), 2) + pow((targetPose.y - robotPose.y), 2));
+    
+    if(pathLeft > 1){    
+        while(distance < offset && pathLeft > 1){
+            path.x.pop(); targetPose.x = path.x.front();
+            path.y.pop(); targetPose.y = path.y.front();  
+            path.z.pop(); targetPose.z = path.z.front();
+            distance = sqrt(pow((targetPose.x - robotPose.x), 2) + pow((targetPose.y - robotPose.y), 2));
+        }
+    }
+    else{
+        targetPose.x = path.x.front();
+        targetPose.y = path.y.front();
+        targetPose.z = path.z.front();
+    }
+
+    // Debug
+    std::cout << "path left = " << pathLeft << std::endl;
+    // std::cout << "robotPose.x = " << robotPose.x << " robotPose.y = " << robotPose.y << " robotPose.theta = " << robotPose.z << std::endl;
+    // std::cout << "targetPose.x = " << targetPose.x << " targetPose.y = " << targetPose.y << " targetPose.theta = " << targetPose.z << std::endl;
+    // std::cout << "distance = " << distance << std::endl;
+
+    return targetPose;
+}
+
+Robot::Pose_t Robot::PointToPointPID(Pose_t robotPose, Pose_t targetPose, float maxSpeed)
+{
+    Pose_t speedRobot;
+    speedRobot.x = speedRobot.y = speedRobot.z = 0;
+
+    float output[3] = {0, 0, 0};
+
+    float proportional[3] = {0, 0, 0};
+    float integral[3] = {0, 0, 0};
+    float derivative [3]= {0, 0, 0};
+
+    float error[3] = {0, 0, 0};
+    static float prevError[3] = {0, 0, 0};
+    static float sumError[3] = {0, 0, 0};
+
+    float kp[3] = {40, 40, 40};
+    float ki[3] = {0, 0, 0};
+    float kd[3] = {0, 0, 0};
+
+    error[0] = targetPose.x - robotPose.x;
+    error[1] = targetPose.y - robotPose.y;
+    error[2] = targetPose.z - robotPose.z; 
+
+    if(abs(error[2]) >= MATH_PI){
+        if(error[2] > 0){
+            error[2] = error[2] - 2*MATH_PI;
+        }
+        else{
+            error[2] = error[2] + 2*MATH_PI;
+        }
+    }
+
+    for(int i=0 ; i<=2 ; i++){
+        sumError[i] += error[i];
+
+        proportional[i] = kp[i] * error[i];
+        integral[i] = ki[i] * sumError[i];
+        derivative[i] = kd[i] * (error[i] - prevError[i]);
+
+        prevError[i] = error[i];
+
+        output[i] = proportional[i] + integral[i] + derivative[i];
+    }
+
+    // Speed Limiter and Normalizer
+    if(abs(output[0]) >= maxSpeed || abs(output[1]) >= maxSpeed || abs(output[2]) >= maxSpeed){
+        float max = 0;
+        for(int i=0 ; i<=2 ; i++){
+            if(abs(output[i]) > max){
+                max = abs(output[i]);
+            }
+        }
+        for(int i=0 ; i<=2 ; i++){
+            output[i] = output[i] * (maxSpeed / max);
+        }
+    }
+
+    // Speed Limiter Only
+    for(int i=0 ; i<=2 ; i++){
+        if(output[i] >= maxSpeed){
+            output[i] = maxSpeed;
+        }
+        else if(output[i] <= -maxSpeed){
+            output[i] = -maxSpeed;
+        }
+    }
+
+    speedRobot.x = output[0];
+    speedRobot.y = output[1];
+    speedRobot.z = output[2];
+
+    return speedRobot;
 }
